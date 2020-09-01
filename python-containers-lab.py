66c0a6b9cc806537f7bd37b03a889928ee938f00
unknown <draz@ualberta.ca>
2020-09-01 13:40:29 -0600

    first commit

---
+++ python-containers-lab.py
@@ -0,0 +1,40 @@
+# # Exercise 1
+students = ['Arthur', 'Henry', 'Steve']
+# print(students[1])
+# print(students[-1])
+
+# # Exercise 2
+foods = ('Pasta', 'Spaghetti', 'Rice')
+# for food in foods:
+#     print(food + " is a good food")
+# print(foods)
+
+
+# Exercise 3
+# for i in range(-2, 0):
+#     print(foods[i])
+
+
+# # Exercise 4
+# home_town = {
+#     'city': 'Arcadia',
+#     'state': 'California',
+#     'population': '58000',
+# }
+
+# print("I was born in " + home_town['city'] +
+#       home_town['state'] + "-population of " + home_town['population'])
+
+
+# Exercise 5
+# for item in home_town:
+#     print(item[1])
+
+# for a, b in home_town.items():
+#     print(a, b)
+
+
+# Exercise 6
+cohort = []
+for name in cohort:
+    cohort.append({'student': 'tina'})
